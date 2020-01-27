b184db21eea7010707b2f9176d7bbbb1fa2a62a2
Wilson Yan <wilson1.yan@berkeley.edu>
2020-01-27 12:06:42 -0800

    better training plots

--- deepul/utils.py
+++ deepul/utils.py
@@ -17,8 +17,11 @@ def savefig(fname, show_figure=True):
 
 def save_training_plot(train_losses, test_losses, title, fname):
     plt.figure()
-    plt.plot(train_losses, label='train loss')
-    plt.plot(test_losses, label='test loss')
+    n_epochs = len(test_losses) - 1
+    x_train = np.linspace(0, n_epochs, len(train_losses))
+    x_test = np.arange(n_epochs + 1)
+    plt.plot(x_train, train_losses, label='train loss')
+    plt.plot(x_test, test_losses, label='test loss')
     plt.legend()
     plt.title(title)
     plt.xlabel('Epoch')
