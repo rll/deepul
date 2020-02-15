5ddcb07522ce6086a02ac29f0efb4f10f0741417
Alex Li <alexlioralexli@gmail.com>
2020-02-14 23:00:33 -0800

    celeba channel permutation bug fixed.

--- deepul/utils.py
+++ deepul/utils.py
@@ -82,7 +82,9 @@ def load_pickled_data(fname, include_labels=False):
         # Binarize MNIST and shapes dataset
         train_data = (train_data > 127.5).astype('uint8')
         test_data = (test_data > 127.5).astype('uint8')
-
+    if 'celeb.pkl' in fname:
+        train_data = train_data[:, :, :, [2, 1, 0]]
+        test_data = test_data[:, :, :, [2, 1, 0]]
     if include_labels:
         return train_data, test_data, data['train_labels'], data['test_labels']
     return train_data, test_data
