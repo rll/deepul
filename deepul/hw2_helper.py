38f1f170c4a755a341dde5e6925cfa7947933c65
Kevin Frans <kevinfrans2@gmail.com>
2024-02-09 09:21:27 -0800

    hw 2 vaes

---
+++ deepul/hw2_helper.py
@@ -0,0 +1,156 @@
+from .utils import *
+
+
+def plot_vae_training_plot(train_losses, test_losses, title, fname):
+    elbo_train, recon_train, kl_train = train_losses[:, 0], train_losses[:, 1], train_losses[:, 2]
+    elbo_test, recon_test, kl_test = test_losses[:, 0], test_losses[:, 1], test_losses[:, 2]
+    plt.figure()
+    n_epochs = len(test_losses) - 1
+    x_train = np.linspace(0, n_epochs, len(train_losses))
+    x_test = np.arange(n_epochs + 1)
+
+    plt.plot(x_train, elbo_train, label='-elbo_train')
+    plt.plot(x_train, recon_train, label='recon_loss_train')
+    plt.plot(x_train, kl_train, label='kl_loss_train')
+    plt.plot(x_test, elbo_test, label='-elbo_test')
+    plt.plot(x_test, recon_test, label='recon_loss_test')
+    plt.plot(x_test, kl_test, label='kl_loss_test')
+
+    plt.legend()
+    plt.title(title)
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    savefig(fname)
+
+
+def sample_data_1_a(count):
+    rand = np.random.RandomState(0)
+    return [[1.0, 2.0]] + (rand.randn(count, 2) * [[5.0, 1.0]]).dot(
+        [[np.sqrt(2) / 2, np.sqrt(2) / 2], [-np.sqrt(2) / 2, np.sqrt(2) / 2]])
+
+
+def sample_data_2_a(count):
+    rand = np.random.RandomState(0)
+    return [[-1.0, 2.0]] + (rand.randn(count, 2) * [[1.0, 5.0]]).dot(
+        [[np.sqrt(2) / 2, np.sqrt(2) / 2], [-np.sqrt(2) / 2, np.sqrt(2) / 2]])
+
+
+def sample_data_1_b(count):
+    rand = np.random.RandomState(0)
+    return [[1.0, 2.0]] + rand.randn(count, 2) * [[5.0, 1.0]]
+
+
+def sample_data_2_b(count):
+    rand = np.random.RandomState(0)
+    return [[-1.0, 2.0]] + rand.randn(count, 2) * [[1.0, 5.0]]
+
+
+def q1_sample_data(part, dset_id):
+    assert dset_id in [1, 2]
+    assert part in ['a', 'b']
+    if part == 'a':
+        if dset_id == 1:
+            dset_fn = sample_data_1_a
+        else:
+            dset_fn = sample_data_2_a
+    else:
+        if dset_id == 1:
+            dset_fn = sample_data_1_b
+        else:
+            dset_fn = sample_data_2_b
+
+    train_data, test_data = dset_fn(10000), dset_fn(2500)
+    return train_data.astype('float32'), test_data.astype('float32')
+
+
+def visualize_q1_data(part, dset_id):
+    train_data, test_data = q1_sample_data(part, dset_id)
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    ax1.set_title('Train Data')
+    ax1.scatter(train_data[:, 0], train_data[:, 1])
+    ax2.set_title('Test Data')
+    ax2.scatter(test_data[:, 0], test_data[:, 1])
+    print(f'Dataset {dset_id}')
+    plt.show()
+
+
+def q1_save_results(part, dset_id, fn):
+    train_data, test_data = q1_sample_data(part, dset_id)
+    train_losses, test_losses, samples_noise, samples_nonoise = fn(train_data, test_data, part, dset_id)
+    print(f'Final -ELBO: {test_losses[-1, 0]:.4f}, Recon Loss: {test_losses[-1, 1]:.4f}, '
+          f'KL Loss: {test_losses[-1, 2]:.4f}')
+
+    plot_vae_training_plot(train_losses, test_losses, f'Q1({part}) Dataset {dset_id} Train Plot',
+                           f'results/q1_{part}_dset{dset_id}_train_plot.png')
+    save_scatter_2d(samples_noise, title='Samples with Decoder Noise',
+                    fname=f'results/q1_{part}_dset{dset_id}_sample_with_noise.png')
+    save_scatter_2d(samples_nonoise, title='Samples without Decoder Noise',
+                    fname=f'results/q1_{part}_dset{dset_id}_sample_without_noise.png')
+
+
+def visualize_colored_shapes():
+    data_dir = get_data_dir(3)
+    train_data, test_data = load_pickled_data(join(data_dir, 'shapes_colored.pkl'))
+    idxs = np.random.choice(len(train_data), replace=False, size=(100,))
+    images = train_data[idxs]
+    show_samples(images, title='Colored Shapes Samples')
+
+
+def visualize_svhn():
+    data_dir = get_data_dir(3)
+    train_data, test_data = load_pickled_data(join(data_dir, 'svhn.pkl'))
+    idxs = np.random.choice(len(train_data), replace=False, size=(100,))
+    images = train_data[idxs]
+    show_samples(images, title='SVHN Samples')
+
+
+def visualize_cifar10():
+    data_dir = get_data_dir(3)
+    train_data, test_data = load_pickled_data(join(data_dir, 'cifar10.pkl'))
+    idxs = np.random.choice(len(train_data), replace=False, size=(100,))
+    images = train_data[idxs]
+    show_samples(images, title='CIFAR10 Samples')
+
+
+def q2_save_results(part, dset_id, fn):
+    assert part in ['a', 'b'] and dset_id in [1, 2]
+    data_dir = get_data_dir(3)
+    if dset_id == 1:
+        train_data, test_data = load_pickled_data(join(data_dir, 'svhn.pkl'))
+    else:
+        train_data, test_data = load_pickled_data(join(data_dir, 'cifar10.pkl'))
+
+    train_losses, test_losses, samples, reconstructions, interpolations = fn(train_data, test_data, dset_id)
+    samples, reconstructions, interpolations = samples.astype('float32'), reconstructions.astype('float32'), interpolations.astype('float32')
+    print(f'Final -ELBO: {test_losses[-1, 0]:.4f}, Recon Loss: {test_losses[-1, 1]:.4f}, '
+          f'KL Loss: {test_losses[-1, 2]:.4f}')
+    plot_vae_training_plot(train_losses, test_losses, f'Q2({part}) Dataset {dset_id} Train Plot',
+                           f'results/q2_{part}_dset{dset_id}_train_plot.png')
+    show_samples(samples, title=f'Q2({part}) Dataset {dset_id} Samples',
+                 fname=f'results/q2_{part}_dset{dset_id}_samples.png')
+    show_samples(reconstructions, title=f'Q2({part}) Dataset {dset_id} Reconstructions',
+                 fname=f'results/q2_{part}_dset{dset_id}_reconstructions.png')
+    show_samples(interpolations, title=f'Q2({part}) Dataset {dset_id} Interpolations',
+                 fname=f'results/q2_{part}_dset{dset_id}_interpolations.png')
+
+
+def q3_save_results(dset_id, fn):
+    assert dset_id in [1, 2]
+    data_dir = get_data_dir(3)
+    if dset_id == 1:
+        train_data, test_data = load_pickled_data(join(data_dir, 'svhn.pkl'))
+    else:
+        train_data, test_data = load_pickled_data(join(data_dir, 'cifar10.pkl'))
+
+    vqvae_train_losses, vqvae_test_losses, prior_train_losses, prior_test_losses, samples, reconstructions = fn(train_data, test_data, dset_id)
+    samples, reconstructions = samples.astype('float32'), reconstructions.astype('float32')
+    print(f'VQ-VAE Final Test Loss: {vqvae_test_losses[-1]:.4f}')
+    print(f'Transformer Prior Final Test Loss: {prior_test_losses[-1]:.4f}')
+    save_training_plot(vqvae_train_losses, vqvae_test_losses,f'Q3 Dataset {dset_id} VQ-VAE Train Plot',
+                       f'results/q3_dset{dset_id}_vqvae_train_plot.png')
+    save_training_plot(prior_train_losses, prior_test_losses,f'Q3 Dataset {dset_id} Transformer Prior Train Plot',
+                       f'results/q3_dset{dset_id}_prior_train_plot.png')
+    show_samples(samples, title=f'Q3 Dataset {dset_id} Samples',
+                 fname=f'results/q3_dset{dset_id}_samples.png')
+    show_samples(reconstructions, title=f'Q3 Dataset {dset_id} Reconstructions',
+                 fname=f'results/q3_dset{dset_id}_reconstructions.png')
